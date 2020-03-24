1f42d4517291ac1f1d180678877c50d12ecb8ffb
Jayendra Kumara <jayendra@clouda.io>
2020-03-24 17:19:11 +0530

    permission table names

--- react/utils/const.ts
+++ react/utils/const.ts
@@ -4,9 +4,9 @@ export const BUSINESS_ROLE = 'BusinessRole'
 export const BUSINESS_ROLE_FIELDS = ['permissions']
 export const BUSINESS_ROLE_SCHEMA = 'business-role-schema-v1'
 
-export const BUSINESS_PERMISSION = 'BusinessRole'
+export const BUSINESS_PERMISSION = 'BusinessPermission'
 export const BUSINESS_PERMISSION_FIELDS = ['id', 'name']
-export const BUSINESS_PERMISSION_SCHEMA = 'business-role-schema-v1'
+export const BUSINESS_PERMISSION_SCHEMA = 'business-permission-schema-v1'
 
 export const ORG_ASSIGNMENT = 'UserOrganization'
 export const ORG_ASSIGNMENT_FIELDS = ['roleId']
