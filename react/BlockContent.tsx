08c2e9a2c291a764b94ce21db6af368878707ed1
Ícaro <icaro.azevedo@vtex.com.br>
2021-02-16 15:53:45 -0300

    Updating CSS Handles to new major

--- react/BlockContent.tsx
+++ react/BlockContent.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import type { PropsWithChildren } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
+import type { CssHandlesTypes } from 'vtex.css-handles'
 
 const CSS_HANDLES = ['challengeContentWrapper'] as const
 
-function BlockContent({ children }: PropsWithChildren<unknown>) {
-  const handles = useCssHandles(CSS_HANDLES)
+interface Props {
+  classes?: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
+}
+
+function BlockContent({ children, classes }: PropsWithChildren<Props>) {
+  const { handles } = useCssHandles(CSS_HANDLES, { classes })
 
   return (
     <div className={`mw9 center ${handles.challengeContentWrapper}`}>
